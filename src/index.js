9a0173bded704768e30382b816f8cb821ebe24c4
Domotiz Arq <davidchicaisa@gmail.com>
2022-10-10 10:34:17 -0500

    probando el enlace a heroku

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ app.use('/api', alarmsRoutes);
 
 app.use(cors()); //habilitar otras aplicaciones para realizar solicitudes 
 
+/*
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:'+port);
   res.header(
@@ -26,7 +27,7 @@ app.use(function(req, res, next) {
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
-});
+});*/
 //routes
 app.get('/', (req, res) => {
   res.send("Panel de alarmas");
