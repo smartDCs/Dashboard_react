14b94d6779edc9b1aea8b4bc5bed66b4cd187b3a
Domotiz Arq <davidchicaisa@gmail.com>
2022-10-10 14:38:43 -0500

    probando el enlace a heroku

--- src/routes/sonoff.js
+++ src/routes/sonoff.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const router=express.Router();
 const sonoffSchema=require("../models/sonoffPow");
-
+const ewelink = require("ewelink-api");
 const cors = require("cors");
-
+require("dotenv").config();
 router.use(cors());
 //save sonoff data
 router.post("/sonoffData",(req,res)=>{
@@ -26,9 +26,61 @@ router.get("/sonoffData",(req,res)=>{
         message:error
     }));
 });
-//read sonoff data base
-router.get("/asonoffData",(req,res)=>{
-   res.send('hola');
+//read sonoff data from fornt end
+
+router.get("/asonoffData",(req,res)=>async function dataEwelink(){
+  // read ewelink data
+const email = process.env.email;
+const password = process.env.password;
+const region = process.env.region;
+
+
+
+  // lee el estado del sonoff
+    const connection = new ewelink({
+      email,
+      password,
+      region,
+    });
+   
+    
+
+        try {
+          const deviceid = '100042b09d';
+          const pow = await connection.getDevice(deviceid);
+          //console.log(pow);
+          const voltaje = (pow['params']['voltage']);
+          const current = (pow['params']['current']);
+          var status = (pow['params']['switch']);
+          console.log("voltaje del sistema " + voltaje);
+          console.log("corriente del sistema " + current);
+          console.log("estado el interruptor " + status);
+          if (status == 'on') {
+            status = 1;
+          } else {
+            status = 0;
+          }
+          const sonoff = {
+            deviceId: '100042b09d',
+            name: "sonoff pow",
+            voltaje: voltaje,
+            current: current,
+            status: status,
+
+          };
+         res.json(sonoff);
+
+
+          
+
+        }
+        catch (error) {
+          console.log(error)
+        }
+
+res.send("hola");
+    
+ 
 });
 //read a sonoff data
 router.get("/sonoffData/:id",(req,res)=>{
