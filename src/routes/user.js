9087b313f18a22515fa65d330b7d4e89ba194507
Domotiz Arq <davidchicaisa@gmail.com>
2022-11-19 10:39:38 -0500

    get users by email

--- src/routes/user.js
+++ src/routes/user.js
@@ -26,7 +26,7 @@ router.get("/usersDash",(req,res)=>{
 });
 
 //get a user
-router.get("/usersDash/:email&password",(req,res)=>{
+router.get("/usersDash/:email:password",(req,res)=>{
   const {email, password}=req.params;
     userSchema
     .findOne({
